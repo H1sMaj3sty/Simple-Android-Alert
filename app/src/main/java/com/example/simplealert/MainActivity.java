0649bedfc663918c97e74ac7459c012bcffb2391
Da Man <akuncadangannn2020@gmail.com>
2023-11-08 09:55:36 +0700

    Second Commit (Added selected spinner to variable)

--- app/src/main/java/com/example/simplealert/MainActivity.java
+++ app/src/main/java/com/example/simplealert/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     EditText nomorTelpon;
     Spinner menu;
+
+    private String pilihanMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +26,21 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.listSpinner, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         menu.setAdapter(adapter);
+
+        menu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                pilihanMenu = menu.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
     }
     public void alertNotif(View view) {
         String nomorTelp = nomorTelpon.getText().toString();
-        String pesan = "Nomor Telp Anda " + nomorTelp + "?" + "\nKlik OK Jika Benar, Klik CANCEL Jika Salah";
+        String pesan = "Nomor Telp " + pilihanMenu + " adalah "+ nomorTelp + "?" + "\nKlik OK Jika Benar, Klik CANCEL Jika Salah";
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("Alert!");
         builder.setMessage(pesan);
